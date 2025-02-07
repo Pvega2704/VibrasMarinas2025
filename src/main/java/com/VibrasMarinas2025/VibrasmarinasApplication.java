e6122f346086e49673b18fb6df0baeefc25e42f9
Pvega2704 <pvega2704@gmail.com>
2025-02-07 09:57:19 -0600

    Tarea 1

---
+++ src/main/java/com/VibrasMarinas2025/VibrasmarinasApplication.java
@@ -0,0 +1,13 @@
+package com.VibrasMarinas2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VibrasmarinasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VibrasmarinasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/VibrasMarinas2025/VibrasmarinasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.VibrasMarinas2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VibrasmarinasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
